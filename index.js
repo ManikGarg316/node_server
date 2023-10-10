0c918c29530c5de3ec8608347f100bc0557ae268
ManikGarg316 <navigarg779@gmail.com>
2023-10-10 11:43:50 +0530

    Adding history API

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const officeDataSchema = new mongoose.Schema({
   chair_id: Number,
   date_created: Date,
   for_date: Date,
-  name: String,
   emp_id: String,
 });
 
@@ -38,24 +37,61 @@ const server = express();
 server.use(cors());
 server.use(bodyParser.json());
 
-server.get("/demo", async (req, res) => {
-  const data = await OfficeData.find();
-  // console.log(data);
+server.get("/demo", async (_, res) => {
+  const currDate = new Date();
+  // console.log(currDate);
+  const forDate = new Date(
+    currDate.getFullYear(),
+    currDate.getMonth(),
+    currDate.getDate() + 1,
+    5,
+    30
+  );
+  // console.log(currDate.getDate());
+  // forDate.setDate(currDate.getDate() + 1);
+  // forDate.setHours(0);
+  // console.log(forDate);
+  const data = await OfficeData.find({ for_date: forDate });
+  console.log(data);
   console.log("data sent");
   res.json(data);
 });
 
 server.post("/demo", async (req, res) => {
   //   console.log(req);
-  console.log("request recieved");
+  // console.log("request recieved");
   // console.log(req.body);
-  const entry = new OfficeData(req.body);
-  await entry.save();
-  res.json("Entry added");
+  const currDate = new Date();
+  // console.log(currDate);
+  const forDate = new Date(
+    currDate.getFullYear(),
+    currDate.getMonth(),
+    currDate.getDate() + 1,
+    5,
+    30
+  );
+  const data = await OfficeData.find({
+    emp_id: req.body.emp_id,
+    for_date: forDate,
+  });
+  if (data.length !== 0) {
+    res.json("REJECTED");
+  } else {
+    const cleanedData = {
+      chair_id: req.body.chair_id,
+      date_created: req.body.date_created,
+      for_date: req.body.for_date,
+      emp_id: req.body.emp_id,
+    };
+    // console.log(cleanedData);
+    const entry = new OfficeData(cleanedData);
+    await entry.save();
+    res.json("ACCEPTED");
+  }
 });
 
 server.post("/login", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   let message = {};
   const data = await UserData.find({ emp_id: req.body.emp_id });
   // console.log(data[0]);
@@ -90,6 +126,19 @@ server.post("/register", async (req, res) => {
     message["user"] = req.body.emp_id;
     message["email"] = data.email;
     // message["cookie_id"] = "some random generated string!";
+  }
+  res.json(message);
+});
+
+server.post("/history", async (req, res) => {
+  console.log(req.body);
+  const data = await OfficeData.find({ emp_id: req.body.id });
+  let message = {};
+  if (data.length === 0) {
+    message["return"] = "NOT_FOUND";
+  } else {
+    message["return"] = "OK";
+    message["data"] = data;
   }
   res.json(message);
 });
